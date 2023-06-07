2c9873e499778b610dd81cdd7a9ab25644265ba9
Anna Eliza <annaeliza.als@gmail.com>
2023-06-07 02:33:19 -0300

    update exemplo

--- exemplo/ordenando.js
+++ exemplo/ordenando.js
@@ -1,132 +1,109 @@
-function autalizarSelect() {
-  let select = document.querySelector('#sortMethod');
-  let optionValue = select.options[select.selectedIndex];
-  let value = optionValue.value;
-  return value;
+function swap(arr, posicao1, posicao2) {
+  const temp = arr[posicao1];
+  arr[posicao1] = arr[posicao2];
+  arr[posicao2] = temp;
 }
 
-let valores = [];
+function shuffle(arr, swaps) {
+  for (let i = 0; i < swaps; i++) {
+    const pos1 = Math.floor(Math.random() * arr.length);
+    const pos2 = Math.floor(Math.random() * arr.length);
+    swap(arr, pos1, pos2);
+  }
+}
+
+function bubbleSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+}
+
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swap(arr, i, minIndex);
+    }
+  }
+}
+
+function quickSort(arr, left, right) {
+  if (left < right) {
+    const pivotIndex = partition(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+}
+
+function partition(arr, left, right) {
+  const pivot = arr[right];
+  let i = left - 1;
+  for (let j = left; j < right; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+      swap(arr, i, j);
+    }
+  }
+  swap(arr, i + 1, right);
+  return i + 1;
+}
 
 function add() {
-  const valorInput = document.getElementById('valor');
-  if (valorInput.value != '') {
-  const valoresList = document.getElementById('valores');
-  const node = document.createElement('li');
-  const textNode = document.createTextNode(valorInput.value);
-  valoresList.appendChild(node);
-  node.appendChild(textNode);
-  valorInput.value = '';
+  const valorInput = document.getElementById("valor");
+  const valor = Number(valorInput.value);
+  if (valor) {
+    const li = document.createElement("li");
+    li.textContent = valor;
+    li.classList.add("valor");
+    document.getElementById("valores").appendChild(li);
+    valorInput.value = "";
   }
+}
+
+function limpar() {
+  document.getElementById("valores").innerHTML = "";
 }
 
 function ordenar() {
-  const listaSelecao = autalizarSelect();
-  const listaValores = document.getElementById('valores');
-  const valores = Array.from(listaValores.children).map(item =>
-  parseInt(item.innerHTML)
-  );
-
-  switch (listaSelecao) {
-  case 'bubble_sort':
-      bubble_sort(valores);
+  const lista = document.getElementById("valores");
+  const vetor = Array.from(lista.children).map(item => parseInt(item.textContent));
+  const algoritmo = document.getElementById("selecao").value;
+  switch (algoritmo) {
+    case "0":
+      bubbleSort(vetor);
       break;
-  case 'selection_sort':
-      selection_sort(valores);
+    case "1":
+      selectionSort(vetor);
       break;
-  case 'quick_sort':
-      quick_sort(valores);
+    case "2":
+      quickSort(vetor, 0, vetor.length - 1);
       break;
-  default:
-      return;
   }
-
-  const newItems = valores
-  .map(valor => `<li>${valor}</li>`)
-  .reduce((acc, item) => acc + item, '');
-
-  listaValores.innerHTML = newItems;
+  const novosItens = vetor.map(item => `<li>${item}</li>`).join("");
+  lista.innerHTML = novosItens;
 }
 
-const misturar = () => {
-  const listaValores = document.getElementById('valores');
-  const valores = Array.from(listaValores.children).map(item =>
-  parseInt(item.innerHTML)
-  );
+function misturar() {
+  const lista = document.getElementById("valores");
+  const vetor = Array.from(lista.children).map(item => parseInt(item.textContent));
+  shuffle(vetor, 10);
+  const novosItens = vetor.map(item => `<li>${item}</li>`).join("");
+  lista.innerHTML = novosItens;
+}
 
-  if (valores != '') {
-  shuffle(valores);
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    add();
   }
-
-  const newItems = valores
-  .map(valor => `<li>${valor}</li>`)
-  .reduce((acc, item) => acc + item, '');
-
-  listaValores.innerHTML = newItems;
-};
-
-const swap = (arr, i, j) => {
-  const temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-};
-
-const shuffle = (arr, numSwaps = 100) => {
-  const len = arr.length;
-  for (let i = 0; i < numSwaps; i++) {
-    const rand1 = Math.floor(Math.random() * len);
-    const rand2 = Math.floor(Math.random() * len);
-  swap(arr, rand1, rand2);
-  }
-};
-
-const bubble_sort = arr => {
-  const len = arr.length;
-  let swapped = false;
-  for (let i = 0; i < len - 1; i++) {
-  swapped = false;
-  for (let j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-      swap(arr, j, j + 1);
-      swapped = true;
-      }
-  }
-  if (!swapped) break;
-  }
-};
-
-const selection_sort = arr => {
-  const len = arr.length;
-  let minIndex;
-  for (let i = 0; i < len - 1; i++) {
-  minIndex = i;
-  for (let j = i + 1; j < len; j++) {
-      if (arr[j] < arr[minIndex]) {
-      minIndex = j;
-      }
-  }
-  if (minIndex !== i) {
-      swap(arr, i, minIndex);
-  }
-  }
-};
-
-const quick_sort = (arr, low = 0, high = arr.length - 1) => {
-  if (low < high) {
-  const pivotIndex = particionamento(arr, low, high);
-  quick_sort(arr, low, pivotIndex - 1);
-  quick_sort(arr, pivotIndex + 1, high);
-  }
-};
-
-const particionamento = (arr, low, high) => {
-  const pivot = arr[high];
-  let i = low - 1;
-  for (let j = low; j < high; j++) {
-  if (arr[j] < pivot) {
-      i++;
-      swap(arr, i, j);
-  }
-  }
-  swap(arr, i + 1, high);
-  return i + 1;
-};
+}
